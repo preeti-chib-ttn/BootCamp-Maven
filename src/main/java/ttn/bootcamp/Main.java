a54dc6d7fa20ac17f7d776c56da656e161308b85
Preeti Chib <preeti.chib@tothenew.com>
2025-03-04 11:47:52 +0530

    Question 6

--- src/main/java/ttn/bootcamp/Main.java
+++ src/main/java/ttn/bootcamp/Main.java
@@ -1,17 +1,6 @@
 package ttn.bootcamp;
-
-/*
-Q1) Following parameters are to be considered for exercise submission: - The complete project has to be submitted. -
-.idea, target directories should be excluded -
-A text file containing the relevant URLs which are referenced and any other documentation.
-Q2) Add a maven dependency and its related repository URL.
-Q3) Add a new repository in the pom.xml and use its dependencies.
-Q4) Using JAR plugin, make changes in the pom.xml to make the jar executable. Using java -jar JAR_NAME, the output should be printed as "Hello World"
-Q5) Differentiate between the different dependency scopes: compile, runtime, test, provided using different dependencies being defined in your pom.xml.
-Q6) Create a multi-module project. Run package command at the top level to make jar of every module.
- */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Spring Assignment");
     }
 }
